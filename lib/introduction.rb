e6983e13e1f72b3c7fb201b7acdb71e58031d232
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-15 22:05:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,9 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(name, language='English')
+introduction_with_language('Danielle', 'Ruby')
+
+introduction_with_language_optional(name, language='English')
 
 
 
